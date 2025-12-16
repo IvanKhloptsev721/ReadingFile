8e65398577f659d8d51f5ad2a7ce60541fcc543d
IvanKhloptsev721 <khloptsev.ivan@gmail.com>
2025-12-16 18:16:40 +0300

    initial

---
+++ ReadingFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace Readling_Writing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              File.AppendAllText("output.text", "\n Привет этот текст добавится в файле\n");
+            string[] lines = File.ReadAllLines("output.text");
+            Console.WriteLine("Содержимое файла:\n");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
